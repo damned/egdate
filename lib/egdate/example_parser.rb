80cfcae580de90ffa8d5a9bb6f191453fa8c3c9a
danm <writetodan@yahoo.com>
2014-09-14 14:09:38 +0100

    @damned split out turning of examples into sequences of literals, digits etc.

--- lib/egdate/example_parser.rb
+++ lib/egdate/example_parser.rb
@@ -1,36 +1,30 @@
+require_relative 'sequencer'
+
 module Eg
   module Date
     class ExampleParser
+      def initialize(sequencer: Sequencer.new)
+        @sequencer = sequencer
+      end
+
       def parse(example)
         return [:day_padded, ' ', :month_3_char, ' ', :year_4_digit] if example.start_with?('0')
         parts = []
-        last_type = nil
-        sequence_length = 0
-        index = 0
-        example.each_char {|c|
-          index += 1
-          last_char = index == example.length
-          if '.-: '.include? c
-            parts << c
-            type = :literal 
-          else
-            type = :digit
-          end
-          if type == last_type
-            sequence_length += 1
-          end
-          if type != last_type || last_char
-            unless last_type.nil?
-              if type == :digit
-                parts << :year_4_digit
-              end
-            end
-            last_type = type
-            sequence_length = 1
-          end
+        sequencer.sequences(example).each { |type_and_sequence|  
+          type, sequence = type_and_sequence
+          if type == :literal
+            parts += sequence.each_char.to_a
+          elsif type == :digit
+            parts << :year_4_digit
+          end 
         }
         parts
       end
+
+      private
+
+      attr_reader :sequencer
+
     end
   end
 end
